cee4a57c9d569129634ff659c080503b0f956487
DuzhakIvan <ivanduzhak@gmail.com>
2022-10-06 17:17:33 +0300

    Добавление функции на проверку максимальной длинны строки. Редактирование выводов в консоль

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
+
 let randomNumber;
 function getRandomNumber(min, max) {
   if (typeof min !== 'number' || typeof max !== 'number') {
@@ -8,11 +9,27 @@ function getRandomNumber(min, max) {
     const temporaryVariable = min;
     min = max;
     max = temporaryVariable;
-    console.log('Значения поменялись местами ' + min + ' ' + max);
+    console.log(`${max} ${min} Значения поменялись местами, теперь ${min} ${max}`);
   }
   randomNumber = min + Math.random() * (max + 1 - min);
   return Math.floor(randomNumber);
 }
 
 randomNumber = getRandomNumber(20, 4);
-console.log(randomNumber);
+console.log(`Случайное число равно ${randomNumber}`);
+
+// Функция для проверки максимальной длины строки.
+
+function isLenghtCorrect (string, maxStringLenght) {
+  const stringLenght = string.length;
+  console.log(`Длина строки ${stringLenght}`);
+  if (stringLenght >= maxStringLenght) {
+    console.log('Длина строки превышает лимит!');
+    return false;
+  }
+  console.log('Длина строки в норме!');
+  return true;
+}
+
+const lenghtCorrect = isLenghtCorrect ('Попробуем так, хз получится ли, но попытка не пытка.', 140);
+console.log(lenghtCorrect);
