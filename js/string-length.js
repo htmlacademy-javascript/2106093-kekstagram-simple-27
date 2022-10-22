b1ab5749ba13d87b97b362a499e94ffa7052597e
DuzhakIvan <ivanduzhak@gmail.com>
2022-10-22 18:43:44 +0300

    Разделение кода на модули. Редактирование кода.

---
+++ js/string-length.js
@@ -0,0 +1,6 @@
+// Функция для проверки максимальной длины строки.
+const stringLength = function isLengthLongCorrect (string, maxStringLength) {
+  return string.length >= maxStringLength;
+};
+
+export {stringLength};
