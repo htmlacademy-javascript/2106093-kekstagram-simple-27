584d711280031977ef531843d8fdea8891943b84
DuzhakIvan <ivanduzhak@gmail.com>
2022-11-12 19:32:03 +0300

    Редактирование 2

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@
 // ВОПРОС
 // ВОПРОС
 // В консоли ошибка GET http://localhost:3000/js/on-apply-effect net::ERR_ABORTED 404 (Not Found) при импорте...
-import { resetEffect } from './on-apply-effect.js';
+// import { resetEffect } from './on-apply-effect.js';
 
 // ---- Переменные ----
 // Переменные для модального окна
@@ -12,7 +12,7 @@ const uploadModal = form.querySelector('.img-upload__overlay'); // Modal
 const closeUploadModalButton = uploadModal.querySelector('.img-upload__cancel'); // Кнопка закрытия Modal
 const overlay = form.querySelector('.img-upload__overlay'); // Overlay
 const uploadImage = document.querySelector('.img-upload__preview').querySelector('img'); // Изображение
-const getSliderContainer = () => form.querySelector('.effect-level');
+const sliderContainer = form.querySelector('.effect-level');
 
 // Переменные комметария
 const imageDescription = form.querySelector('.text__description');
@@ -31,7 +31,7 @@ const onModalKeydown = (evt) => {
 // Функция открытия Modal
 function openModal () {
   uploadModal.classList.remove('hidden');
-  resetEffect();
+  // resetEffect();
   // Вызов обработчика для закрытия Modal по клавише
   document.addEventListener('keydown', onModalKeydown);
   document.addEventListener('click', clickOutModal);
@@ -60,7 +60,7 @@ function closeModal () {
   document.removeEventListener('keydown', onModalKeydown);
   // Установка параметров по умолчанию.
   resetForm();
-  resetEffect();
+  // resetEffect();
 }
 
 //Обработчик для открытия Modal по кнопке
@@ -69,4 +69,4 @@ upload.addEventListener('change', () => openModal ());
 // Обработчик для закрытия Modal по кнопке
 closeUploadModalButton.addEventListener('click', closeModal);
 
-export {form, imageDescription, closeModal, closeUploadModalButton, onModalKeydown, isEscapeKey, uploadImage, getSliderContainer};
+export {form, imageDescription, closeModal, closeUploadModalButton, onModalKeydown, isEscapeKey, uploadImage, sliderContainer};
